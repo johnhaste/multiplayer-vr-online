0b63743c032a25cda807df3709278fedb2d75375
John Haste <joaovictorspteixeira@gmail.com>
2022-06-06 17:25:36 -0300

    Simple UI Interaction Added

---
+++ Assets/CanvasInteraction.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class CanvasInteraction : MonoBehaviour
+{
+ 
+    public TextMeshProUGUI simpleUIText;
+    public Button buton1;
+ 
+    public void OnButton1Clicked()
+    {
+        //simpleUIText.text = "Button1 is clicked";
+        buton1.GetComponent<TextMeshPro>().text = "Was clicked";
+    }
+ 
+}
