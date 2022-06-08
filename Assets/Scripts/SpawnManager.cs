bb401b66e91a95ec3f9f030cf514e45374915632
John Haste <joaovictorspteixeira@gmail.com>
2022-06-07 21:08:25 -0300

    Spawn Generic VR Player with Photon

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject GenericVRPlayerPrefab;
+
+    private Vector3 spawnPosition = new Vector3(0f,1f,0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PhotonNetwork.IsConnectedAndReady)
+        {
+            PhotonNetwork.Instantiate(GenericVRPlayerPrefab.name, spawnPosition, Quaternion.identity );
+        }
+    }
+
+}
